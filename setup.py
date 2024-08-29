c387797d5e581633bee365065899ce6649f96673
Sunnykumar926 <118978144+Sunnykumar926@users.noreply.github.com>
2024-08-29 14:37:39 +0530

    Added all the file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='water potability test using data and code versioning',
+    author='sunny',
+    license='MIT',
+)
